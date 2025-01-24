f8589801bdf8fc1a8bf3f7af83b6da10855e2421
Ned <nedeeshaw@virginmedia.com>
2025-01-24 16:26:03 +0000

    update streamlit deployment app

--- src/app.py
+++ src/app.py
@@ -9,40 +9,57 @@ __version__ = "0.1"
 
 import streamlit as st
 
-from src.chatbot import chatbot, set_up_chatbot_workflow
+from src.chatbot import ChatbotWorkflow
 
 st.title("Splitwise Chatbot")
+st.markdown("Ask me questions about your Splitwise data!")
 st.logo(image="src/logo.png")
 st.markdown(
     """
     <style>
     img[data-testid="stLogo"] {
-            height: 3.5rem;
+            height: 12rem;
     }
     </style>
     """,
     unsafe_allow_html=True,
 )
-# Initialize session state
-if "initialized" not in st.session_state:
-    st.session_state.messages = []
-    st.session_state.initialized = False
-
 # Splitwise ID input
-group_id = st.number_input("Enter your Splitwise Group ID:", min_value=1)
+group_id = st.number_input("Enter your Splitwise Group ID:", value=50024800)
 
-if st.button("Initialize Chat") and not st.session_state.initialized:
-    set_up_chatbot_workflow(group_id=group_id)
-    st.session_state.initialized = True
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if st.session_state.initialized:
-    # Display chat history
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
+# Display chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Chat input
-    if prompt := st.chat_input("Ask about your Splitwise expenses"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        response = chatbot(prompt)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+if "chatbot" not in st.session_state:
+    st.session_state.chatbot = ChatbotWorkflow(group_id)
+    # Show an initial message from the chatbot displaying all users and categories
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": "Show me all the possible users and categories in this group",
+        }
+    )
+    with st.chat_message("assistant"):
+        response = st.session_state.chatbot.stream(
+            "Show me all the possible users and categories in this group"
+        )
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+# Chat input
+if prompt := st.chat_input("Write your message here..."):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Get response from chatbot
+    with st.chat_message("assistant"):
+        response = st.session_state.chatbot.stream(prompt)
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
